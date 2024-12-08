00a72ab405ca1d39497b686f2d41a811ecdd7aa4
Soumyadeep <umyadeepb27@gmail.com>
2024-12-08 14:25:18 +0530

    time mode added

--- src/app.ts
+++ src/app.ts
@@ -22,17 +22,174 @@ interface charArr {
 }
 
 program
-    .option('-c, --count <num>', 'number of words');
+    .option('-m, --mode <flag>', 'mode')
+    .option('-c, --count <num>', 'number of words')
+    .option('-t, --time <num>', 'time in seconds');
 
 program.parse();
 
 const options = program.opts();
-readKeys(options.count ? options.count : null);
+if(options.mode) {
+    if(options.mode === "count") {
+        countMode(options.count ? parseInt(options.count) : null);
+    } else if(options.mode === "time"){
+        timeMode(options.time ? parseInt(options.time) : 30);
+    }
+} else {
+    // todo: add proper error msgs
+    console.error('Mode is required');
+}
+
 // console.log(drawBox("Sample text"));
 
-async function readKeys(count: string = null) {
+async function timeMode(time: number) {
     let sampleWords: string[] = [];
-    let wordCount: number = (count ? parseInt(count) : 10);
+    let wordCount: number = 30;
+    if(wordCount > 100 || wordCount < 1) {
+        console.log('\x1b[31mPlease enter valid word count[1-100]\x1b[0m');
+        process.exit();
+    }
+    // let wordCount = 3;
+    let userInp: string[] = [];
+    let startTime: number = null;
+
+    let curWordIndex: number = 0;
+    let backLimit: number = 0;
+
+    for (let i: number = 0; i < wordCount; i++) {
+        sampleWords.push(enData[Math.floor(Math.random() * enData.length)]);
+    }
+    // sampleWords = ['apple', 'ball', 'cat'];
+    readline.emitKeypressEvents(process.stdin);
+    process.stdin.setRawMode(true);
+    process.stdin.on('keypress', async (str: string, key: Key) => {
+        if (startTime === null) {
+            startTime = new Date().getTime();
+            setTimeout(() => {
+                endBlitz(startTime, userInp.length);
+            }, time * 1000)
+        }
+        if (key.ctrl && key.name === 'c') {
+            process.exit();
+        } else {
+            if (str === " ") {
+                if (userInp.length === 0) {
+                    return;
+                } else {
+                    if (curWordIndex === backLimit && userInp[curWordIndex] === sampleWords[curWordIndex])
+                        backLimit++;
+                    curWordIndex++;
+                    userInp[curWordIndex] = "";
+                }
+            } else if (str === "\b") {
+                if (userInp.length === 0) {
+                    return;
+                } else {
+                    if (userInp[curWordIndex].length === 0) {
+                        if (curWordIndex === backLimit) {
+                            return;
+                        } else {
+                            userInp.splice(curWordIndex, 1);
+                            curWordIndex--;
+                        }
+                    } else {
+                        userInp[curWordIndex] = (userInp[curWordIndex] ? userInp[curWordIndex].slice(0, -1) : str);
+                    }
+                }
+            } else if(/^[a-zA-Z0-9_]*$/.test(str)) {
+                userInp[curWordIndex] = (userInp[curWordIndex] ? userInp[curWordIndex] + str : str);
+            }
+
+            let resString: string = "";
+            let resArr: charArr[][] = [[]]; 
+            let wIndex = 0;
+        
+            let cursorSet: boolean = false;
+            for (let i: number = 0; i < sampleWords.length; i++) {
+                if (i > userInp.length - 1) {
+                    if (!cursorSet) {
+                        cursorSet = true;
+                        resArr[wIndex].push({chr: "|", code: 'nil'});
+                    }
+                    resString = resString + sampleWords[i];
+                    for(const n of sampleWords[i]) {
+                        resArr[wIndex].push({chr: n, code: 'nil'});
+                    };
+                } else {
+                    if (userInp[i].length >= sampleWords[i].length) {
+                        for (let j = 0; j < userInp[i].length; j++) {
+                            if (j < sampleWords[i].length) {
+                                if (sampleWords[i][j] === userInp[i][j]) {
+                                    resArr[wIndex].push({chr: userInp[i][j], code: 'grn'});
+                                } else {
+                                    resArr[wIndex].push({chr: sampleWords[i][j], code: 'red'});
+                                }
+                            } else {
+                                resArr[wIndex].push({chr: userInp[i][j], code: 'mgn'});
+                            }
+                        }
+                        if (!cursorSet && i === userInp.length - 1 && i === curWordIndex) {
+                            cursorSet = true;
+                            resArr[wIndex].push({chr: "|", code: 'nil'});
+                        }
+                    } else {
+                        for (let j: number = 0; j < sampleWords[i].length; j++) {
+                            if (j < userInp[i].length) {
+                                if (sampleWords[i][j] === userInp[i][j]) {
+                                    resArr[wIndex].push({chr: sampleWords[i][j], code: 'grn'});
+                                } else {
+                                    resArr[wIndex].push({chr: sampleWords[i][j], code: 'red'});
+                                }
+                            } else {
+                                if (!cursorSet && i === curWordIndex) {
+                                    cursorSet = true;
+                                    resArr[wIndex].push({chr: "|", code: 'nil'});
+                                }
+                                if (i < curWordIndex) {
+                                    resArr[wIndex].push({chr: sampleWords[i][j], code: 'gry'});
+                                } else {
+                                    resArr[wIndex].push({chr: sampleWords[i][j], code: 'nil'});
+                                }
+                            }
+                        }
+                    }
+                }
+
+                if (i < sampleWords.length - 1) {
+                    resArr.push([]);
+                    wIndex++;
+                }
+            }
+            console.clear();
+            const d: string = await figlet('Cursor Blitz');
+            console.log(d);
+            logInBox(["Countdown started for "+time+" seconds."]);
+            const formattedArr: string[] = formatCharArr(resArr);
+            logInBox(formattedArr);
+
+            if (userInp.length >  sampleWords.length - 10) {
+                for (let i: number = 0; i < 10; i++) {
+                    sampleWords.push(enData[Math.floor(Math.random() * enData.length)]);
+                }
+            }
+        }
+    });
+    let resString: string = "|";
+    for (let i: number = 0; i < sampleWords.length; i++) {
+        resString = resString + sampleWords[i];
+        if (i < sampleWords.length - 1)
+            resString = resString + " ";
+    }
+    console.clear();
+    const d: string = await figlet('Cursor Blitz');
+    console.log(d);
+    logInBox(["Timer starts when you start typing. You have "+time+" seconds."]);
+    console.log(resString);
+}
+
+async function countMode(count: number = null) {
+    let sampleWords: string[] = [];
+    let wordCount: number = (count ? count : 10);
     if(wordCount > 100 || wordCount < 1) {
         console.log('\x1b[31mPlease enter valid word count[1-100]\x1b[0m');
         process.exit();
@@ -147,13 +304,12 @@ async function readKeys(count: string = null) {
             console.clear();
             const d: string = await figlet('Cursor Blitz');
             console.log(d);
+            logInBox(["Type "+wordCount+" words to finish blitz."]);
             const formattedArr: string[] = formatCharArr(resArr);
             logInBox(formattedArr);
 
             if (userInp.length === sampleWords.length && sampleWords[wordCount - 1] === userInp[wordCount - 1]) {
-                const endTime: number = new Date().getTime();
-                printEndScreen(startTime, endTime, wordCount);
-                process.exit();
+                endBlitz(startTime, wordCount);
             }
         }
     });
@@ -166,14 +322,21 @@ async function readKeys(count: string = null) {
     console.clear();
     const d: string = await figlet('Cursor Blitz');
     console.log(d);
+    logInBox(["Type "+wordCount+" words to finish blitz."]);
     console.log(resString);
+}
+
+function endBlitz(startTime: number, wordsTyped: number) {
+    const endTime: number = new Date().getTime();
+    printEndScreen(startTime, endTime, wordsTyped);
+    process.exit();
 }
 
 function printEndScreen(startTime: number, endTime: number, wordCount: number) {
     let result: string[] = [];
     const timeDiff: number = (endTime - startTime) / 1000;
 
-    result.push('Congrats, you\'ve completed the blitz');
+    result.push('Congrats, you\'ve completed the blitz.');
     result.push(`Time: ${(Math.round(timeDiff * 100) / 100)} seconds`);
     result.push(`Speed: ${Math.floor((wordCount / timeDiff) * 60)} WPM`);
 
